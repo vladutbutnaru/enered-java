41e871fa442dfc7032c620aa59021c8f95cec753
vladbutnaru <vlad2me@gmail.com>
2016-05-03 20:44:32 +0300

    Added files for the first Enered session.

---
+++ Resources/Session 1/SumExample.java
@@ -0,0 +1,13 @@
+   public static void main(String[] args) {
+       //declaration
+	    int a;
+        int b;
+        int sum;
+       //assignment
+        a = 3;
+        b = 6;
+       //operation
+        sum = a + b;
+       //display in the console
+        System.out.println(sum);
+    }
