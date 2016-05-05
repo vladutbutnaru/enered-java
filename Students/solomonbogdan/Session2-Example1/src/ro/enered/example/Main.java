801152ee18aff690ebeed671e187c9be2a93c1ee
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2016-05-05 17:25:53 +0300

    Added exercise files for Session 2

---
+++ Students/solomonbogdan/Session2-Example1/src/ro/enered/example/Main.java
@@ -0,0 +1,55 @@
+package ro.enered.example;
+
+public class Main {
+	public static void main(String args[]){
+		// In acest exemplu vom exersa lucrul cu primitivele din limbajul Java, cu operatiile de baza, cu conditionalele, cu structurile repetitive si cu vectorii
+		
+		//Exemplul 1: Declarati 2 numere intregi si afisati suma lor
+		
+		
+		//Exemplul 2: Declarati 4 numere reale si afisati rezultatul inmultirii lor
+		
+		
+		//Exemplul 2b: Declarati 2 numere intregi si afisati catul si restul impartirii lor
+		
+		
+		//Exemplul 3: Declarati 2 numere intregi pozitive si unul negativ si afisati rezultatul adunarii primelor 2, inmultit cu al treilea numar (cel negativ)
+		
+		
+		//Exemplul 4: Declarati 2 variabile de tip String si concatenati-le, astfel incat sa afisati textul "Ma numesc " + nume + " si invat " + curs
+		
+		
+		//Exemplul 5: Declarati 2 numere intregi si afisati-l pe cel mai mare
+		
+		
+		//Exemplul 6: Declarati 2 numere intregi si afisati textul "Numarul " + nr1 " este mai mare decat numarul " + nr2, in functie de relatia dintre cele 2
+		
+		
+		//Exemplul 7: Declarati o variabile de tip boolean care sa fie true daca cele doua numere declarate in exemplul anterior sunt egale
+		
+		
+		//Exemplul 8: Determinati si afisati rezultatul ridicarii la putere a doua numere intregi (declarate de voi), folosind operatiunea "for"
+		
+		
+		//Exemplul 8b: Declarati un numar intreg si gasiti si afisati, printr-o instructiune repetitiva si una de decizie, cel mai mare numar par, mai mic sau egal cu cel initial
+		
+		
+		//Exemplul 9: Declarati 2 numere intregi si verificati rezultatul adunarii lor astfel: daca este 4, atunci afisati "Numerele sunt bune", daca este 8, afisati "Numerele sunt okay", iar in orice alt caz afisati "Numere nu sunt bune"
+		
+		
+		//Exemplul 10: Declarati un vector de numere intregi de dimensiune 8. Adaugati, utilizand o instructiune repetitiva, numerele de la 1 la 8, in ordine crescatoare
+		
+		
+		//Exemplul 10b: Afisati vectorul declarat anterior. Apoi afisati-l in ordinea inversa a elementelor (utilizand o instructiune repetitiva)
+		
+		
+		//Exemplul 11: Declarati un vector de numere reale de dimensiune 4. Adaugati manual 4 elemente in acest vector si afisati-l.
+		
+		
+		//Exemplul 12: Cautati si afisati cel mai mare element din vectorul declarat anterior.
+	
+		
+		
+		
+	}
+}
